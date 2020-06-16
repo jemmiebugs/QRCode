944e25a629d8d6cb30496e8ca17d12331ef5e3ba
Jeminat Muritala <40564213+jemmiebugs@users.noreply.github.com>
2020-06-16 22:46:27 +0100

    Add files via upload

---
+++ SemicolonQRCode.java
@@ -0,0 +1,65 @@
+package com.jemmiebugs;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+
+public class SemicolonQRCode {
+
+    public static void main(String[] args) {
+        String myCodeText = "https://semicolon.africa/";
+        String filePath = "./SemicolonQR.png";
+        int size = 250;
+        String fileType = "png";
+        File myFile = new File(filePath);
+        try {
+
+            Map<EncodeHintType, Object> hintMap = new EnumMap<>(EncodeHintType.class);
+            hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+            hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size,
+                    size, hintMap);
+            int SemicolonWidth = byteMatrix.getWidth();
+            BufferedImage image = new BufferedImage(SemicolonWidth, SemicolonWidth,
+                    BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, SemicolonWidth, SemicolonWidth);
+            graphics.setColor(Color.BLACK);
+
+            for (int i = 0; i < SemicolonWidth; i++) {
+                for (int j = 0; j < SemicolonWidth; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            ImageIO.write(image, fileType, myFile);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n\nYou have successfully created QR Code.");
+    }
+}
